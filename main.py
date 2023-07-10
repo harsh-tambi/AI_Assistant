7454d8563de4845fdccfdd28623592bd37c9c4e9
Harsh Tambi <ht2293@nyu.edu>
2023-07-10 12:03:50 +0530

    eee

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 import openai
+import speech_recognition as sr
+import wikipedia
+import os
+
+def say(text):
+    os.system(f"say {text}")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        query = r.recognize_google(audio, language = 'en-in')
+        print (f"User Said - {query}")
+        return query
+
+if __name__ == '__main__':
+    say('Hi! How can I help you?')
+    print ("You can speak now...... I'm listening")
+    text = takeCommand()
+    say ('Audio Recieved')
+    say(text)
+
+
+
